f0ced345e3d0759d5b110a2e3788563dffeafcec
ddouglasz <douglasegiemeh@gmail.com>
2021-08-22 00:11:29 +0100

    initial json data for customer data

---
+++ src/api/customerData.ts
@@ -0,0 +1,8 @@
+import { ICustomerData } from './customerData.types'
+
+export const customerData: ICustomerData[] = [{
+    customer: {
+        name: 'Douglas Penguin',
+        feedback: ['good good', 'bad bad']
+    }
+}]
