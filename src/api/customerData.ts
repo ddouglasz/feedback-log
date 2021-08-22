9371f4e05936088cde7e5711838cf7283d360f23
ddouglasz <douglasegiemeh@gmail.com>
2021-08-22 22:32:17 +0100

    initial structure of customer data and feedback

--- src/api/customerData.ts
+++ src/api/customerData.ts
@@ -1,8 +1,16 @@
-import { ICustomerData } from './customerData.types'
+import { ICustomerData } from '../types/customerData.types'
 
-export const customerData: ICustomerData[] = [{
-    customer: {
-        name: 'Douglas Penguin',
-        feedback: ['good good', 'bad bad']
-    }
-}]
+export const customerData: ICustomerData[] = [
+    {
+        customer: {
+            name: 'Douglas Penguin',
+            feedback: ['good good', 'bad bad']
+        }
+    },
+    {
+        customer: {
+            name: 'Ukung Seadog',
+            feedback: ['Phew! good good', 'bad bado baddest']
+        }
+    },
+]
