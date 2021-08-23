55c844fc3e47c70602df6f18a39cd66554b8321b
ddouglasz <douglasegiemeh@gmail.com>
2021-08-23 21:46:13 +0100

    reduce customer seeded data to just one item in customer data array

--- src/api/customerData.ts
+++ src/api/customerData.ts
@@ -7,12 +7,5 @@ export const customerData: ICustomerData[] = [
             name: 'Douglas Penguin',
             feedback: ['good good', 'bad bad']
         }
-    },
-    {
-        customer: {
-            id: 2,
-            name: 'Ukung Seadog',
-            feedback: ['Phew! good good', 'good better great']
-        }
-    },
+    }
 ]
