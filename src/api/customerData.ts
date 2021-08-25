8dcb436fde05c6bb7045a76b803f3a76fa5780dc
ddouglasz <douglasegiemeh@gmail.com>
2021-08-25 10:11:13 +0100

    modify data structure

--- src/api/customerData.ts
+++ src/api/customerData.ts
@@ -2,10 +2,8 @@ import { ICustomerData } from '../types/customerData.types'
 
 export const customerData: ICustomerData[] = [
     {
-        customer: {
-            id: 1,
-            name: 'Douglas Penguin',
-            feedback: ['good good', 'bad bad']
-        }
+        id: 1,
+        name: 'Douglas Penguin',
+        feedback: ['good good', 'bad bad']
     }
 ]
